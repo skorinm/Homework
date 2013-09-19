4964a9870c41e4e23511211376b3813e9d8ccd2e
root <root@beaglebone.(none)>
2013-09-19 21:36:31 +0000

    temp loop bash script

---
+++ Homework3/temp-loop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Running this program will spit out the temp of both temp sensors 
+mounted on the board.
+# 9/19/13 Matt Skorina
+
+while true; do
+	temp_48=`i2cget -y 1 0x48 0`
+	temp_49=`i2cget -y 1 0x49 0`
+
+	temp_48=`echo $((temp_48))*1.8+32 | bc`
+	echo "Inner sensor is" $temp_48 "F"
+
+	temp_49=`echo $((temp_49))*1.8+32 | bc`
+	echo "Outer sensor is" $temp_49 "F"
+
+	sleep 1
+	clear
+done
